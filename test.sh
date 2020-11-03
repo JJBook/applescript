8bf5eea134bc57f9542c805bbe65e650b28ee117
纪承琼 <597684882@qq.com>
2020-11-03 14:45:06 +0800

    first commit

---
+++ test.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# osascript -e 'set volume output volume 50' # 1-100
+# osascript -e 'say "支付宝收款2千万元整"'
+
+# osascript <<END
+
+# tell application "App Store"
+# 	activate
+# end tell
+
+# tell application "Safari"
+# 	tell front document
+# 		name
+# 	end tell
+# end tell
+
+
+# tell application "Safari"
+#     tell front document
+#         activate
+#     end tell
+# end tell
+
+# tell app "System Events" to display dialog "Hello World"
+
+# tell application "QQ"
+	
+# end
+
+# set "abc" to clipboard
+
+
+
+
+# END
+
+
+# name=$(basename "$1")
+# osascript <<EOT
+# display dialog "
+# MD5:
+# $(md5 "$1" | awk '{print $NF}')
+
+# SHA1:
+# $(openssl sha1 "$1" | awk '{print $NF}')
+
+# SHA256:
+# $(openssl dgst -sha256 "$1" | awk '{print $NF}')
+
+# SHA512:
+# $(openssl dgst -sha512 "$1" | awk '{print $NF}')" with title "$(echo $name)" with icon Caution
+# EOT
+
+
+# osascript << EOT
+# tell application "Safari" to activate
+# 	end tell
+# end tell
+# EOT
+
+# osascript << EOT
+
+# tell application "System Events"
+#     tell process "QQ" -- 告诉 Money Pro
+#         entire contents -- 获取所有 UI 元素
+#     end tell
+# end tell
+
+# EOT
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
